e3cb527709e5c125533989242d97f08f6970e617
Asif R Choudhury <choudhury.asif@gmail.com>
2014-03-06 13:58:03 -0600

    Main class created

---
+++ src/com/asifrc/ttt/Main.java
@@ -0,0 +1,15 @@
+package com.asifrc.ttt;
+
+/**
+ * Created by archoud on 3/6/14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Main().go();
+    }
+
+    public void go() {
+        
+    }
+}
+
